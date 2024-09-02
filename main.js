e59abfb19761cc1adc1646e308eacbcdb58d0de6
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2024-09-02 14:52:58 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+const slides = document.querySelectorAll(".slide");
+const btns = document.querySelectorAll(".btn");
+
+let currentSlide = 1;
+
+const manualNav = (manual) => {
+
+
+  
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+
+  });
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  slides[manual].classList.add("active");
+  btns[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
+
+const repeat = (activeClass) => {
+  let active = document.getElementsByClassName("active");
+  let i = 0;
+
+  const repeater = () => {
+    setTimeout(() => {
+      [...active].forEach((activeSlide)=>{
+        activeSlide.classList.remove('active')
+      })
+      
+      slides[i].classList.add("active");
+      btns[i].classList.add("active");
+      i++;
+
+      if (slides.length == i) {
+        i = 0;
+      }
+      if (i >= slides.length) {
+        return;
+      }
+      repeater();
+    }, 5000);
+  };
+  repeater()
+};
+
+repeat();
